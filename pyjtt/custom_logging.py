784f10ec5bca267209772733aa8e4c9e985f1659
Nikolay Golub <nikolay.v.golub@gmail.com>
2013-02-13 10:19:52 +0400

    Small fixes in logging module

--- pyjtt/custom_logging.py
+++ pyjtt/custom_logging.py
@@ -6,7 +6,7 @@ import logging.handlers
 import ConfigParser
 
 config_filename = 'pyjtt_app.cfg'
-config = ConfigParser.SafeConfigParser({'loglevel' : 'INFO'})
+config = ConfigParser.SafeConfigParser({'loglevel': 'INFO'})
 try:
     config.read(config_filename)
     loglevel = config.get('main', 'loglevel', 'DEBUG').upper()
@@ -27,7 +27,7 @@ except ConfigParser.NoOptionError:
 LOG_FILENAME = 'pyjtt.log'
 logger = logging.getLogger('pyjtt_logger')
 logger.setLevel(getattr(logging, loglevel))
-formatter =  logging.Formatter(fmt='%(asctime)s %(levelname)s %(message)s')
+formatter = logging.Formatter(fmt='%(asctime)s %(levelname)s %(message)s')
 rotater = logging.handlers.RotatingFileHandler(
     LOG_FILENAME, mode='a', encoding='utf-8', maxBytes=10485760, backupCount=5)
 rotater.setFormatter(formatter)
