838fc344136dac28dff8d52bb2512df5d906976f
Nikolay Golub <nikolay.v.golub@gmail.com>
2013-06-03 10:16:28 +0400

    Update gui button

--- pyjtt/gui/login_screen.py
+++ pyjtt/gui/login_screen.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'login_screen.ui'
 #
-# Created: Sun Jun  2 10:05:17 2013
+# Created: Mon Jun  3 10:15:38 2013
 #      by: PyQt4 UI code generator 4.10.1
 #
 # WARNING! All changes made in this file will be lost!
--- pyjtt/gui/pyjtt_icons_rc.py
+++ pyjtt/gui/pyjtt_icons_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Sun Jun 2 10:05:17 2013
+# Created: Mon Jun 3 10:15:39 2013
 #      by: The Resource Compiler for PyQt (Qt v4.8.4)
 #
 # WARNING! All changes made in this file will be lost!
