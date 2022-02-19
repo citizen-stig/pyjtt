cc9330a70e64d0974e8352be6123ba54f9f3746a
Nikolai Golub <nikolay.v.golub@gmail.com>
2022-02-19 19:47:13 +0100

    Clean ups

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import os
 from cx_Freeze import setup, Executable
 
 options = {}
+NAME = 'pyJTT'
 
 path = ["pyjtt"] + sys.path
 icon_path = os.path.join("resources", "icons", "icon.icns")
@@ -19,31 +20,31 @@ build_exe_options = {
 
 # http://msdn.microsoft.com/en-us/library/windows/desktop/aa371847(v=vs.85).aspx
 shortcut_table = [
-    ("DesktopShortcut",  # Shortcut
-     "DesktopFolder",  # Directory_
-     "pyJTT",  # Name
-     "TARGETDIR",  # Component_
+    ("DesktopShortcut",       # Shortcut
+     "DesktopFolder",         # Directory_
+     NAME,                    # Name
+     "TARGETDIR",             # Component_
      "[TARGETDIR]pyjtt.exe",  # Target
-     None,  # Arguments
-     None,  # Description
-     None,  # Hotkey
-     None,  # Icon
-     None,  # IconIndex
-     None,  # ShowCmd
-     '%APPDATA%\pyjtt'  # WkDir
+     None,                    # Arguments
+     None,                    # Description
+     None,                    # Hotkey
+     None,                    # Icon
+     None,                    # IconIndex
+     None,                    # ShowCmd
+     '%APPDATA%\pyjtt'        # WkDir
      ),
-    ("ProgramMenuShortcut",  # Shortcut
-     "ProgramMenuFolder",  # Directory_
-     "pyJTT",  # Name
-     "TARGETDIR",  # Component_
+    ("ProgramMenuShortcut",   # Shortcut
+     "ProgramMenuFolder",     # Directory_
+     "pyJTT",                 # Name
+     "TARGETDIR",             # Component_
      "[TARGETDIR]pyjtt.exe",  # Target
-     None,  # Arguments
-     None,  # Description
-     None,  # Hotkey
-     None,  # Icon
-     None,  # IconIndex
-     None,  # ShowCmd
-     '%APPDATA%\pyjtt'  # WkDir
+     None,                    # Arguments
+     None,                    # Description
+     None,                    # Hotkey
+     None,                    # Icon
+     None,                    # IconIndex
+     None,                    # ShowCmd
+     '%APPDATA%\pyjtt'        # WkDir
      )]
 
 # Now create the table dictionary
@@ -57,13 +58,15 @@ bdist_msi_options = {'data': msi_data}
 # GUI applications require a different base on Windows (the default is for a
 # console application).
 base = None
+target_name = 'pyjtt'
 if sys.platform == "win32":
     base = "Win32GUI"
     options["build_exe"] = build_exe_options
     options["bdist_msi"] = bdist_msi_options
+    target_name = 'pyjtt.exe'
 elif sys.platform == "darwin":
     bdist_mac_options = {
-        "bundle_name": "pyJTT",
+        "bundle_name": NAME,
         "iconfile": "resources/icons/icon.icns",
         "include_resources": [
             ("resources/icons/start.ico", "resources/icons/start.ico"),
@@ -90,12 +93,12 @@ setup(name="pyjtt",
       description="Jira Time Tracker",
       maintainer="Nikolai Golub",
       maintainer_email="nikolay.v.golub@gmail.com",
-      long_description="Allows track time in JIRA online and manage worklogs",
+      long_description="Allows track time in JIRA online and manage work logs",
       license="GNU GENERAL PUBLIC LICENSE Version 3",
       options=options,
       executables=[Executable(target_app,
                               base=base,
-                              targetName="pyjtt.exe",
+                              targetName=target_name,
                               icon=icon_path,
-                              shortcutName="pyJTT",
+                              shortcutName=NAME,
                               shortcutDir="DesktopFolder")])
